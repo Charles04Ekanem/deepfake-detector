cd2aee1e4c1547c4ab20a8fb3047f5221e404826
Charles Ekanem <charlesitaekanem@gmail.com>
2025-04-20 01:41:57 +0100

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,18 +1,26 @@
 import torch
 from facenet_pytorch import InceptionResnetV1
 from torchvision import transforms
-from PIL import Image
+import gdown
+import os
 
 device = torch.device('cpu')
 
-# Load model
-def load_model(model_path):
+MODEL_PATH = "best_model.pth"
+DRIVE_FILE_ID = "1-Uc3_jm0-_LkV0otAn9osBD3ra44hC0T"
+
+def download_model_if_needed():
+    if not os.path.exists(MODEL_PATH):
+        url = f"https://drive.google.com/uc?id={DRIVE_FILE_ID}"
+        gdown.download(url, MODEL_PATH, quiet=False)
+
+def load_model():
+    download_model_if_needed()
     model = InceptionResnetV1(classify=True, num_classes=2).to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     model.eval()
     return model
 
-# Predict image
 def predict_image(model, image):
     transform = transforms.Compose([
         transforms.Resize((160, 160)),
@@ -23,3 +31,4 @@ def predict_image(model, image):
     output = model(img)
     _, predicted = torch.max(output, 1)
     return "Real" if predicted.item() == 0 else "Fake"
+
