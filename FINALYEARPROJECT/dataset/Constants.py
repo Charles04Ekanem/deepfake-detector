ef41cd3e3e8229b33774a61a64f3ef7997fd5c84
Charles Ekanem <charlesitaekanem@gmail.com>
2025-06-21 02:41:27 +0100

    Update Constants.py

--- FINALYEARPROJECT/dataset/Constants.py
+++ FINALYEARPROJECT/dataset/Constants.py
@@ -1,13 +1,17 @@
 import os
-DATA_FOLDER = r"C:\Users\CHARLES EKANEM\Documents\FINALYEARPROJECT\dataset"
-ORIGINAL_VIDEOS_FOLDER = r"C:\Users\CHARLES EKANEM\Documents\FINALYEARPROJECT\dataset\Real_videos"
-FAKE_VIDEOS_FOLDER = r"C:\Users\CHARLES EKANEM\Documents\FINALYEARPROJECT\dataset\Fake_videos"
-FACES_FOLDER = r"C:\Users\CHARLES EKANEM\Documents\FINALYEARPROJECT\dataset\faces"
-FACES_REAL = r"C:\Users\CHARLES EKANEM\Documents\FINALYEARPROJECT\dataset\faces\real"
-FACES_FAKE = r"C:\Users\CHARLES EKANEM\Documents\FINALYEARPROJECT\dataset\faces\fake"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_FOLDER = os.path.join(BASE_DIR, "dataset")
+ORIGINAL_VIDEOS_FOLDER = os.path.join(DATA_FOLDER, "original_sequences", "youtube", "raw", "videos")
+FAKE_VIDEOS_FOLDER = os.path.join(DATA_FOLDER, "manipulated_sequences", "DeepFakeDetection", "raw", "videos")
+FACES_FOLDER = os.path.join(DATA_FOLDER, "faces")
+FACES_REAL = os.path.join(FACES_FOLDER, "real")
+FACES_FAKE = os.path.join(FACES_FOLDER, "fake")
 FACES_CSV = os.path.join(FACES_FOLDER, "balanced_faces_temp.csv")
 
 print("Original videos:", ORIGINAL_VIDEOS_FOLDER)
 print("Fake videos:", FAKE_VIDEOS_FOLDER)
 print("Faces will be saved in:", FACES_REAL, "and", FACES_FAKE)
 print("Metadata CSV will be:", FACES_CSV)
+
